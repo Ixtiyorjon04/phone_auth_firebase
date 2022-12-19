539dfa529fd9378b90d9fbac8012375b3494024d
abc <ixtiyorjonisacoff@gmail.com>
2022-12-19 22:40:11 +0500

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:phone_auth_firebase/provider/auth_provider.dart';
+import 'package:phone_auth_firebase/screens/welcome_screen.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context,listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title:  Text("Flutter Auth Phone"),
+      actions: [
+        IconButton(onPressed: (){
+          ap.userSignOut().then((value) => Navigator.push(context, MaterialPageRoute(builder: (context)=>WelcomeScreen())));
+        }, icon: Icon(Icons.exit_to_app))
+      ],
+      ),
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.purple,
+            backgroundImage: NetworkImage(ap.userModel.profilePic),
+            radius: 50,
+          ),
+          SizedBox(height: 20,),
+          Text(ap.userModel.name),
+          Text(ap.userModel.phoneNumber),
+          Text(ap.userModel.email),
+          Text(ap.userModel.bio),
+
+        ],
+      )),
+    );
+  }
+}
